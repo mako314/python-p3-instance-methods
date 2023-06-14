13f546c73fb213ca6ca3dbdc37b3fdde60a1c6d9
Macolister <bispo.mac@gmail.com>
2023-06-14 13:31:50 -0400

    write class methods

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,15 @@
 class Person:
     # Class body goes here
 
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
     #Instance method definition
     pass
+
+mako = Person()
+
